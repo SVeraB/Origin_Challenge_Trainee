6454e6038631c57838674aa05c6d7b09efb3c19c
SVeraB <svera@fi.uba.ar>
2023-12-28 11:18:11 -0300

    Creacion repositorio

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import * as bootstrap from 'bootstrap'
+import './index.css'
+import { BrowserRouter} from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
